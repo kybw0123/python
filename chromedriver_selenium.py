f649925a4d548d1e4c36a16a97cd44aa075f3e57
YongBae Kim <73388879+kybw0123@users.noreply.github.com>
2020-10-27 21:38:59 +0900

    selenium search
    셀레니움을 이용해 구글 검색 및 검색의 갯수 확인 그리고 기존 검색어를 삭제하고 새로운 검색 후 검색 결과 확인

--- chromedriver_selenium.py
+++ chromedriver_selenium.py
@@ -9,14 +9,29 @@ driver.implicitly_wait(3)
 
 # URL 접속 후 python 검색
 driver.get('https://google.com')
-driver.find_element_by_name("q").send_keys('python')
+driver.find_element_by_name("q").send_keys('트럼프')
 driver.find_element_by_name('btnK').click()
 
 driver.implicitly_wait(3)
 htmls = driver.page_source
 soup = BeautifulSoup(htmls, 'html.parser')
-python_name = soup.select('div.yuRUbf > a > h3 > span')
-
+python_name = soup.select('#result-stats')
 # 출력하기
 for n in python_name:
+    print('트럼프')
+    print(n.text.strip())
+
+# 검색어 삭제 후 새로운 검색
+driver.find_element_by_name("q").clear()
+driver.find_element_by_name('q').send_keys('바이든')
+driver.find_element_by_class_name('Tg7LZd').click()
+
+driver.implicitly_wait(3)
+htmls2 = driver.page_source
+soup2 = BeautifulSoup(htmls2, 'html.parser')
+python_name2 = soup2.select('#result-stats')
+
+# 출력하기
+for n in python_name2:
+    print('바이든')
     print(n.text.strip())
