eab95dfcfa67c9581bd8267f98eef8148ba2fcf3
YongBae Kim <73388879+kybw0123@users.noreply.github.com>
2020-10-29 21:56:56 +0900

    mobile_game_search.py update
    구글스토어에 접속해서 인기 앱/게임, 유료게임, 최고 매출게임의 게임명과 회사명을 출력하도록 만들었다.

--- mobile_game_search.py
+++ mobile_game_search.py
@@ -8,14 +8,39 @@ driver = webdriver.Chrome('/Users/YB/Downloads/chromedriver')
 driver.implicitly_wait(3)
 
 # URL 접속
-driver.get('http://www.gevolution.co.kr/rank/aos')
+driver.get('https://play.google.com/store/apps/top/category/GAME?hl=ko')
 driver.implicitly_wait(3)
-'''
+
 # 페이지 소스 받아서 인기 게임 이름 출력하기
 soup = BeautifulSoup(driver.page_source, 'lxml')
-#print(soup)
-i = 1
-for data in soup.findAll(name="span", attrs={'class':'rank1', 'alt':'개발사 정보'} ):
-    print(str(i) + '위 : '+data.text.strip())
-    i += 1
-'''
+
+i = 0
+# 게임명, 회사명 리스트로 저장할 준비
+name_result =[]
+company_result = []
+name = soup.findAll('div', attrs={'class':'k6AFYd'})
+company = soup.findAll(attrs={'class': 'b8cIId ReQCgd KoLSrc'})
+
+# for문을 이용해서 미리 변수에 값 저장
+for data in name:
+    name_result.append(data.a.text)
+
+for data2 in company:
+    company_result.append(data2.a.text)
+
+# 출력하기
+while i < len(name_result):
+    if i == 0 :
+        print('------인기 앱/게임------')
+    elif i == 10:
+        print('')
+        print('------인기 유료 게임------')
+    elif i == 20:
+        print('')
+        print('------최고 매출 게임------')
+
+    print('게임명 : ' + name_result[i])
+    print('회사명 : ' + company_result[i])
+    print('')
+
+    i = i + 1
